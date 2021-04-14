c8e1af2a8b295da6e437910c651944853054af48
Rajiv Kumar 4 <sendmailrajiv@gmail.com>
2021-04-14 13:44:19 +0530

    Update version_check.sh

--- version_check.sh
+++ version_check.sh
@@ -4,7 +4,7 @@
 # Author : Rajiv Kumar                                                                #
 # Reviewed By: DevOps Team                                                            #
 #######################################################################################
-SVERSION=${bamboo_maven_version}
+SVERSION=`xml2 < pom.xml  | grep /project/version= | sed 's/.*=//'`
 major=$(echo $SVERSION | cut -c1)
 var2=$(echo $SVERSION | cut -c2)
 miner=$(echo $SVERSION | cut -c3)
@@ -15,7 +15,7 @@ versionLabel=$major$var2$miner$var4$bug
 echo Current Version Name: $SVERSION
 echo Updated Version: $versionLabel
 
-BRANCH_NAME=${bamboo_planRepository_branch}
+BRANCH_NAME=${GITHUB_REF_NAME}
 VERSION=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q |egrep -iwo SNAPSHOT)
 LN=$(grep -n "<version>" pom.xml | head -1 | awk -F ":" '{print $1}')
 MergeCommit=`git log -1 |egrep -iwo Merge | tail -1`
@@ -33,7 +33,7 @@ if [ "${BRANCH_NAME}" = "main" ];
           git config --global user.name "conrkuma"
           git commit -a -m 'AutoCommit remove SNAPSHOT from pom.xml'
           git remote add central ${bamboo_planRepository_repositoryUrl}
-          git push central ${BRANCH_NAME}
+          git push central ${GITHUB_REF_NAME}
           ls -ltr
           echo "The SNAPSHOT Version was available in master. We have removed it from master branch. aborting the build & auto trigger the build again"
           exit 1
