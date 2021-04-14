827c208a1dddb202b0ddf7541032276fa190dea5
Rajiv Kumar 4 <sendmailrajiv@gmail.com>
2021-04-14 13:33:58 +0530

    Update version_check.sh

--- version_check.sh
+++ version_check.sh
@@ -4,7 +4,7 @@
 # Author : Rajiv Kumar                                                                #
 # Reviewed By: DevOps Team                                                            #
 #######################################################################################
-SVERSION=`xml2 < pom.xml  | grep /project/version= | sed 's/.*=//'`
+SVERSION=${bamboo_maven_version}
 major=$(echo $SVERSION | cut -c1)
 var2=$(echo $SVERSION | cut -c2)
 miner=$(echo $SVERSION | cut -c3)
@@ -15,12 +15,12 @@ versionLabel=$major$var2$miner$var4$bug
 echo Current Version Name: $SVERSION
 echo Updated Version: $versionLabel
 
-BRANCH_NAME=${GITHUB_REF}
+BRANCH_NAME=${bamboo_planRepository_branch}
 VERSION=$(mvn -Dexec.executable='echo' -Dexec.args='${project.version}' --non-recursive exec:exec -q |egrep -iwo SNAPSHOT)
 LN=$(grep -n "<version>" pom.xml | head -1 | awk -F ":" '{print $1}')
 MergeCommit=`git log -1 |egrep -iwo Merge | tail -1`
 
-if [ "${BRANCH_NAME}" = "refs/heads/main" ];
+if [ "${BRANCH_NAME}" = "main" ];
   then
     if [ "${VERSION}" = "SNAPSHOT" ];
     then
@@ -29,11 +29,14 @@ if [ "${BRANCH_NAME}" = "refs/heads/main" ];
         then
           echo This is Merge Commit Updating the pom Version
           sed -i "$LN s/$SVERSION/$versionLabel/" pom.xml
-          git config --global user.email "sendmailrajiv@gmail.com"
-          git config --global user.name "rkum79"
+          git config --global user.email "rajiv_kumar@waters.com"
+          git config --global user.name "conrkuma"
           git commit -a -m 'AutoCommit remove SNAPSHOT from pom.xml'
-          git push
+          git remote add central ${bamboo_planRepository_repositoryUrl}
+          git push central ${BRANCH_NAME}
           ls -ltr
+          echo "The SNAPSHOT Version was available in master. We have removed it from master branch. aborting the build & auto trigger the build again"
+          exit 1
         else
           echo Please update the pom.xml file with SNAPSHOT in the version in ${BRANCH_NAME} Branch
           ls -ltr
@@ -54,4 +57,3 @@ if [ "${BRANCH_NAME}" = "refs/heads/main" ];
       exit 1
     fi
 fi
-
